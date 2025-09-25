bf40cd50e8fb5b9acceb5f1e9c4412765f84ab75
jennavahviala <jenna.vahviala@student.laurea.fi>
2025-09-25 18:36:38 +0300

    kuvakansio

--- WS04_DOM/javakoodit.js
+++ WS04_DOM/javakoodit.js
@@ -18,5 +18,13 @@ function loremipsum() {
 function valintaAlert() {
     const valinta = document.getElementById("mySelect").value;
     alert("Valitsit: " + valinta);
-}
 
+    const carImages = {
+        BMW: "https://www.bmw.fi/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/navigation/bmw-8series-coupe-modellfinder.png",
+        Audi: "https://upload.wikimedia.org/wikipedia/commons/9/9d/2017_Audi_A4_Sport_2.0_Front.jpg",
+        Mercedes: "https://upload.wikimedia.org/wikipedia/commons/9/9a/2021_Mercedes-Benz_E_220_d_AMG_Line_Premium_Automatic_2.0_Front.jpg",
+        Volvo: "https://upload.wikimedia.org/wikipedia/commons/8/8d/2018_Volvo_S90_Inscription_T5_Automatic_2.0_Front.jpg"
+    };
+
+    document.getElementById("carimages").src = carImages[valinta];
+}
