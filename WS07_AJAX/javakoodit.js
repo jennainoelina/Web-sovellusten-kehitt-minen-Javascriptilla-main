8c38fa567e3239db404edc4cfb697ba4ced960c5
jennavahviala <jenna.vahviala@student.laurea.fi>
2025-10-26 14:21:40 +0200

    harjoitus 2

--- WS07_AJAX/javakoodit.js
+++ WS07_AJAX/javakoodit.js
@@ -1,3 +1,4 @@
+// HARJOITUS 1
 var jsonteksti = '{ "tyontekijat" : [' +
 			'{ "etunimi":"John" , "sukunimi":"Doe", "ika":30, "kaupunki":"New York" },' +
 			'{ "etunimi":"Anna" , "sukunimi":"Smith", "ika":25, "kaupunki":"London" },' +
@@ -9,6 +10,7 @@ const nappulanimet = document.getElementById("nimet");
 const nappulatiedot = document.getElementById("tiedot");
 const tulostusalue = document.getElementById("jsondata");
 
+// NAPPULA NIMET
 nappulanimet.addEventListener("click", function() {
     let tulostus = "";
 
@@ -21,6 +23,7 @@ nappulanimet.addEventListener("click", function() {
     tulostusalue.innerHTML = tulostus;
 });
 
+// NAPPULA KAIKKI TIEDOT
 nappulatiedot.addEventListener("click", function() {
     let tulostus = "";
 
@@ -34,4 +37,59 @@ nappulatiedot.addEventListener("click", function() {
         "Kaupunki: " + tyontekija.kaupunki + "<br><br>";
     }
     tulostusalue.innerHTML = tulostus;
+});
+
+// HARJOITUS 2
+const raakanappula = document.getElementById("lataaraaka");
+const parsittunappula = document.getElementById("lataaparsi");
+const raakatulostus = document.getElementById("rawdata");
+
+// RAAKANAPPULA
+raakanappula.addEventListener("click", function(){
+    fetch("http://www.omdbapi.com/?s=star+wars&apikey=cbbc6750")
+
+    .then(function(vastaus) {
+        return vastaus.text();
+    })
+    .then(function(data) {
+        raakatulostus.textContent = data;
+    })
+    .catch(function(virhe) {
+        raakatulostus.innerHTML = "Virhe datan latauksessa: " + virhe;
+    })
+});
+
+// PARSITTU NAPPULA
+parsittunappula.addEventListener("click", function() {
+
+    raakatulostus.innerHTML = "Ladataan ja parsitaan dataa...";
+
+    fetch("http://www.omdbapi.com/?s=star+wars&apikey=cbbc6750")
+
+        .then(function(vastaus) {
+            return vastaus.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            
+            let taulukko =
+                "<table border='1' cellpadding='6'><tr><th>Elokuvan nimi</th><th>Vuosi</th><th>Kuva</th></tr>";
+
+            for (let indeksi = 0; indeksi < data.Search.length; indeksi++) {
+                let elokuva = data.Search[indeksi];
+
+                taulukko += "<tr>";
+                taulukko += "<td>" + elokuva.Title + "</td>";
+                taulukko += "<td>" + elokuva.Year + "</td>";
+                taulukko += "<td><img src='" + elokuva.Poster + "' alt='Kuva puuttuu' height='120'></td>";
+                taulukko += "</tr>";
+            }
+
+            taulukko += "</table>";
+
+            raakatulostus.innerHTML = taulukko;
+        })
+        .catch(function(virhe) {
+            raakatulostus.innerHTML = "Virhe datan latauksessa: " + virhe;
+        });
 });
